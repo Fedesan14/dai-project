b3b3096dcd9da3df4de1915b6d79f98fb659c6ac
Fedesan <federicosan140200@gmail.com>
2023-05-14 11:25:57 -0300

    First commit

---
+++ src/test/java/com/grupo9/tpintegrador/TpintegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo9.tpintegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpintegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
