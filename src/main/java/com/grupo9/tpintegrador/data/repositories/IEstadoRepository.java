3dfa1bca72834461acd5cebf958303276ff4a866
Fedesan <federicosan140200@gmail.com>
2023-06-26 22:17:35 -0300

    ABM de la entity Estado

---
+++ src/main/java/com/grupo9/tpintegrador/data/repositories/IEstadoRepository.java
@@ -0,0 +1,12 @@
+package com.grupo9.tpintegrador.data.repositories;
+
+import com.grupo9.tpintegrador.data.models.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface IEstadoRepository extends JpaRepository<Estado, UUID> {
+    boolean existsByNombre(String nombre);
+}
